2f2e3a3a431f182a99248446f9d6f3a7708190f5
m.vovan2010@googlemail.com <m.vovan2010@googlemail.com@4db3a512-df6a-4815-df6a-009ea6533f98>
2015-02-11 09:31:12 +0000

    refactoring

--- cc-uniapp-2015/src/com/uni/cc_uniapp_2015/UniAdresses.java
+++ cc-uniapp-2015/src/com/uni/cc_uniapp_2015/UniAdresses.java
@@ -3,6 +3,7 @@ package com.uni.cc_uniapp_2015;
 import java.util.Locale;
 
 import com.uni.cc_uniapp_2015.util.GPSFinder;
+import com.uni.cc_uniapp_2015.util.Navigation;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -22,71 +23,28 @@ public class UniAdresses extends ActionBarActivity{
 	double LATITUDE;
 	double LONGLATITUDE;
 	
-
+	Navigation navigation;
+	
 	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.show_uni_adresses);
-		
-		//Beziehe Standort
-		GPSFinder tracker = new GPSFinder(this);
-		LATITUDE = tracker.getLatitude();
-		LONGLATITUDE = tracker.getLongitude();
-		
-		
-
-
+		navigation = new Navigation(this);
 	}
 	public void onButtonClick(View view){
 		String uri = null;
 		
 		if(view.getId() == R.id.btn_toIngSchool){
-			
-			//Wenn Position abrufbar -> Übergebe Position an GoogleMaps
-			if(LATITUDE != 0.0 | LONGLATITUDE != 0.0){
-				uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f&daddr=%s", LATITUDE, LONGLATITUDE, ADRESS_INGSchool);
-				//Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-				//startActivity(intent);
-				Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-				startActivity(intent);
-			}
-			
-			//Ist Position nicht abrufbar -> Setze in Maps nur ein Label und lasse den Aktuellen Standort eingeben
-			else{
-				 //uri = String.format(Locale.ENGLISH, "geo:0,0?q=%s", ADRESS_HoPla);
-				Toast.makeText(getApplicationContext(), "FEHLER: Schalten Sie Mobile Daten und GPS ein!",Toast.LENGTH_LONG).show();
-				finish();
-			}
-			
+			this.navigation.navigateTo(ADRESS_INGSchool);	
 		}
 		else if(view.getId() == R.id.btn_toHoPla){
 			
-			//Toast.makeText(getApplicationContext(), (" "+LATITUDE),Toast.LENGTH_LONG).show();
-			if(LATITUDE != 0.0 | LONGLATITUDE != 0.0){
-				 uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f&daddr=%s", LATITUDE, LONGLATITUDE, ADRESS_HoPla);
-				 Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-					startActivity(intent);
-			}
-			else{
-				 //uri = String.format(Locale.ENGLISH, "geo:0,0?q=%s", ADRESS_HoPla);
-				Toast.makeText(getApplicationContext(), "FEHLER: Schalten Sie Mobile Daten und GPS ein!",Toast.LENGTH_LONG).show();
-				finish();
-			}
-			
+			this.navigation.navigateTo(ADRESS_HoPla);
+		
 		}
 		else{
 			
-			//Toast.makeText(getApplicationContext(), " "+LATITUDE,Toast.LENGTH_LONG).show();
-			if(LATITUDE != 0.0 | LONGLATITUDE != 0.0){
-				uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?saddr=%f,%f&daddr=%s", LATITUDE, LONGLATITUDE, ADRESS_AVZ);
-				Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-				startActivity(intent);
-			}
-			else{
-				//uri = String.format(Locale.ENGLISH, "geo:0,0?q=%s", ADRESS_AVZ);
-				Toast.makeText(getApplicationContext(), "FEHLER: Schalten Sie Mobile Daten und GPS ein!",Toast.LENGTH_LONG).show();
-				finish();
-			}
+			this.navigation.navigateTo(ADRESS_AVZ);
 		}
 		//Intent intent =new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
 		//startActivity(intent);
